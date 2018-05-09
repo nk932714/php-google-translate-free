febd7903324307a4f0b1a819858eb757ad4a3fb1
nk <nk932714@gmail.com>
2018-05-09 15:45:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,10 @@ require_once ('vendor/autoload.php');
 use \Statickidz\GoogleTranslate;
 $source = '#'; // This means Auto detect langauge alternatively we can use " auto "
 $target = 'hi';
-$target2 = 'en';
 $text = $_GET["word"];
 //$text = 'verdadero';
 
 $trans = new GoogleTranslate();
 $result = $trans->translate($source, $target, $text);
-$result2 = $trans->translate($source, $target2, $text);
-
-echo $result."<br>";
-echo $result2;
-
+echo $result;
 ?>
